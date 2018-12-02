96182fd50d5ae51e2c0d5980c365971707216bcc
David Bernard <david.bernard.31@gmail.com>
2018-12-02 12:21:23 +0100

    :art: (e2e) compare content of file as string (vs vec[u8]) to ease debug

--- tests/dir_diff.rs
+++ tests/dir_diff.rs
@@ -6,8 +6,9 @@ use self::spectral::prelude::*;
 use self::walkdir::{DirEntry, WalkDir};
 use failure::Error;
 use std::cmp::Ordering;
-use std::fs::File;
-use std::io::prelude::*;
+use std::fs;
+// use std::fs::File;
+// use std::io::prelude::*;
 use std::path::Path;
 
 /// Are the contents of two directories same?
@@ -29,9 +30,12 @@ pub fn is_same<A: AsRef<Path>, B: AsRef<Path>>(a_base: A, b_base: B) -> Result<b
             .that(&a.file_name())
             .is_equal_to(&b.file_name());
         if a.file_type().is_file() {
+            // asserting(&format!("test content of {:?} vs {:?}", a, b))
+            //     .that(&read_to_vec(a.path())?)
+            //     .is_equal_to(&read_to_vec(b.path())?);
             asserting(&format!("test content of {:?} vs {:?}", a, b))
-                .that(&read_to_vec(a.path())?)
-                .is_equal_to(&read_to_vec(b.path())?);
+                .that(&fs::read_to_string(a.path())?)
+                .is_equal_to(&fs::read_to_string(b.path())?);
         }
     }
 
@@ -49,11 +53,11 @@ fn compare_by_file_name(a: &DirEntry, b: &DirEntry) -> Ordering {
     a.file_name().cmp(&b.file_name())
 }
 
-fn read_to_vec<P: AsRef<Path>>(file: P) -> Result<Vec<u8>, Error> {
-    let mut data = Vec::new();
-    let mut file = File::open(file.as_ref())?;
+// fn read_to_vec<P: AsRef<Path>>(file: P) -> Result<Vec<u8>, Error> {
+//     let mut data = Vec::new();
+//     let mut file = File::open(file.as_ref())?;
 
-    file.read_to_end(&mut data)?;
+//     file.read_to_end(&mut data)?;
 
-    Ok(data)
-}
+//     Ok(data)
+// }

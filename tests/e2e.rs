3f8ebe832e000a0cecc10da2863d8cd6edd1d2dc
David Bernard <david.bernard.31@gmail.com>
2020-03-01 10:38:44 +0100

    :truck: moving every content of it tests under a `tests/data`

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -8,12 +8,12 @@ use test_generator::test_resources;
 
 mod dir_diff;
 
-#[test_resources("tests/test_*")]
+#[test_resources("tests/data/test_*")]
 fn test_local_sample_keep(dir_name: &str) {
     assert!(test_local_sample_impl(dir_name, "keep").is_ok());
 }
 
-#[test_resources("tests/test_*")]
+#[test_resources("tests/data/test_*")]
 fn test_local_sample_override(dir_name: &str) {
     assert!(test_local_sample_impl(dir_name, "override").is_ok());
 }
@@ -75,8 +75,8 @@ fn empty_template() -> Result<(), Box<dyn Error>> {
 fn test_1_subfolder() -> Result<(), Box<dyn Error>> {
     let source_subfolder = "dir_1";
     let tmp_dir = tempdir()?;
-    let template_path = PathBuf::from("./tests/test_1/template");
-    let expected_path = PathBuf::from("./tests/test_1/expected").join(source_subfolder);
+    let template_path = PathBuf::from("./tests/data/test_1/template");
+    let expected_path = PathBuf::from("./tests/data/test_1/expected").join(source_subfolder);
     let actual_path = tmp_dir.path().to_path_buf();
 
     Command::cargo_bin(env!("CARGO_PKG_NAME"))?
@@ -103,7 +103,7 @@ fn test_1_subfolder() -> Result<(), Box<dyn Error>> {
 #[test]
 fn test_1_remote_master() -> Result<(), Box<dyn Error>> {
     let tmp_dir = tempdir()?;
-    let expected_path = PathBuf::from("./tests/test_1/expected");
+    let expected_path = PathBuf::from("./tests/data/test_1/expected");
     let actual_path = tmp_dir.path().to_path_buf();
 
     Command::cargo_bin(env!("CARGO_PKG_NAME"))?
@@ -128,7 +128,7 @@ fn test_1_remote_master() -> Result<(), Box<dyn Error>> {
 #[test]
 fn test_1_remote_commitsha1() -> Result<(), Box<dyn Error>> {
     let tmp_dir = tempdir()?;
-    let expected_path = PathBuf::from("./tests/test_1/expected");
+    let expected_path = PathBuf::from("./tests/data/test_1/expected");
     let actual_path = tmp_dir.path().to_path_buf();
 
     Command::cargo_bin(env!("CARGO_PKG_NAME"))?
@@ -155,7 +155,7 @@ fn test_1_remote_commitsha1() -> Result<(), Box<dyn Error>> {
 #[test]
 fn test_1_remote_tag() -> Result<(), Box<dyn Error>> {
     let tmp_dir = tempdir()?;
-    let expected_path = PathBuf::from("./tests/test_1/expected");
+    let expected_path = PathBuf::from("./tests/data/test_1/expected");
     let actual_path = tmp_dir.path().to_path_buf();
 
     Command::cargo_bin(env!("CARGO_PKG_NAME"))?
@@ -182,7 +182,7 @@ fn test_1_remote_tag() -> Result<(), Box<dyn Error>> {
 #[test]
 fn log_should_report_error() -> Result<(), Box<dyn Error>> {
     let tmp_dir = tempdir()?;
-    let sample_path = PathBuf::from("tests/log_error");
+    let sample_path = PathBuf::from("tests/data/log_error");
     let template_path = sample_path.join("template");
     let actual_path = tmp_dir.path().join("my-project").to_path_buf();
 

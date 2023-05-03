ec8002bbeb756e75a5db5111f8dcf8de2c1bb522
Igor Laborie <igor.laborie@wefox.com>
2023-05-03 16:45:21 +0200

    :bug: Scripts now inherit I/O
    Fix #627

--- src/scripts.rs
+++ src/scripts.rs
@@ -1,5 +1,5 @@
 use crate::error::*;
-use run_script::ScriptOptions;
+use run_script::{IoOptions, ScriptOptions};
 
 #[derive(Debug, Default, Clone, Deserialize, PartialEq, Eq, Hash, PartialOrd, Ord)]
 #[serde(deny_unknown_fields, default)]
@@ -12,7 +12,9 @@ pub struct Script {
 impl Script {
     pub(crate) fn run(&self) -> Result<()> {
         if let Some(cmd) = &self.cmd {
-            let options = ScriptOptions::new();
+            let mut options = ScriptOptions::new();
+            options.input_redirection = IoOptions::Inherit;
+            options.output_redirection = IoOptions::Inherit;
             let args = vec![];
             run_script::run(cmd, &args, &options).map_err(|source| Error::ScriptError {
                 script: cmd.clone(),
@@ -20,5 +22,44 @@ impl Script {
             })?;
         }
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::Write;
+
+    use run_script::IoOptions;
+    use similar_asserts::assert_eq;
+
+    use super::*;
+
+    #[test]
+    fn should_redirect_io() {
+        let cmd = r#"
+            echo "What's your name?"
+            read username;
+            echo "👋 Hello ${username}"
+            echo "🚨 Plaf!"  1>&2"#;
+
+        let mut options = ScriptOptions::new();
+        options.input_redirection = IoOptions::Pipe;
+        options.output_redirection = IoOptions::Pipe;
+        let args = vec![];
+
+        // let mut input = stdin().lock();
+        let mut child = run_script::spawn(cmd, &args, &options).unwrap();
+
+        let mut stdin = child.stdin.take().unwrap();
+        std::thread::spawn(move || {
+            stdin.write_all("ffizer".as_bytes()).unwrap();
+        });
+
+        let out = child.wait_with_output().unwrap();
+
+        let stdout = String::from_utf8_lossy(&out.stdout);
+        assert_eq!(stdout, "What's your name?\n👋 Hello ffizer\n");
+        let stderr = String::from_utf8_lossy(&out.stderr);
+        assert_eq!(stderr, "🚨 Plaf!\n");
     }
 }
